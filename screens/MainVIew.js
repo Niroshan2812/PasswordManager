c477f60a0712eba2f6c337997ea1243e8a76453e
Niroshan <niroshan9812@gmail.com>
2024-10-23 12:10:00 +0530

    initial

---
+++ screens/MainVIew.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {View} from "react-native";
+
+
+const  MainView = ()=>{
+    return(
+
+    <View>
+        <Text> Main View </Text>
+    </View>
+    );
+}
+
+export default MainView;
